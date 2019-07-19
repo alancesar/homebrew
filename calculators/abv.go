637685e843df798f92da7f95e35c929edd238b91
Alan Cesar Elias <alancesarelias@gmail.com>
2019-07-19 19:48:16 -0300

    Abv calculator
    Signed-off-by: Alan Cesar Elias <alancesarelias@gmail.com>

---
+++ calculators/abv.go
@@ -0,0 +1,19 @@
+package calculators
+
+import "homebrew/entities"
+
+type AbvCalculator struct{}
+
+func (calculator AbvCalculator) Calculate(recipe entities.Recipe) entities.Abv {
+	ogInSg := recipe.Og.Sg.Value
+	fgInSg := recipe.Fg.Sg.Value
+	abv := (((76.08 * (ogInSg - fgInSg)) / (1.775 - ogInSg)) * (fgInSg / 0.794)) / 100
+	abw := abv * 0.8
+	attenuation := (ogInSg - fgInSg) / (ogInSg - 1)
+
+	return entities.Abv{
+		Abv:         abv,
+		Abw:         abw,
+		Attenuation: attenuation,
+	}
+}
