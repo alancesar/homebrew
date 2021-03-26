b77903e907728380cb17e1a361f2399354feea32
Alan Cesar Elias <alan.elias@pismo.io>
2021-03-26 19:08:20 -0300

    Add Pressure units

---
+++ pressure/pressure.go
@@ -0,0 +1,36 @@
+package pressure
+
+const (
+	psiInKgfcm2 = 14.223
+	barInKgfcm2 = 1.01972
+)
+
+type Pressure struct {
+	PSI    float64
+	Kgfcm2 float64
+	Bar    float64
+}
+
+func NewFromPSI(value float64) Pressure {
+	return Pressure{
+		PSI:    value,
+		Kgfcm2: value / 14.223,
+		Bar:    value / barInKgfcm2 / psiInKgfcm2,
+	}
+}
+
+func NewFromKgfcm2(value float64) Pressure {
+	return Pressure{
+		PSI:    value * psiInKgfcm2,
+		Kgfcm2: value,
+		Bar:    value / barInKgfcm2,
+	}
+}
+
+func NewFromBar(value float64) Pressure {
+	return Pressure{
+		PSI:    value * barInKgfcm2 * psiInKgfcm2,
+		Kgfcm2: value * barInKgfcm2,
+		Bar:    value,
+	}
+}
---
+++ pressure/pressure_test.go
@@ -0,0 +1,96 @@
+package pressure
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewFromPSI(t *testing.T) {
+	type args struct {
+		value float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want Pressure
+	}{
+		{
+			name: "Should create from PSI properly",
+			args: args{
+				value: 1,
+			},
+			want: Pressure{
+				PSI:    1,
+				Kgfcm2: 0.07030865499542993,
+				Bar:    0.06894898108836733,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewFromPSI(tt.args.value); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewFromPSI() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewFromKgfcm2(t *testing.T) {
+	type args struct {
+		value float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want Pressure
+	}{
+		{
+			name: "Should create from Kgf/cm² properly",
+			args: args{
+				value: 1,
+			},
+			want: Pressure{
+				PSI:    14.223,
+				Kgfcm2: 1,
+				Bar:    0.9806613580198487,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewFromKgfcm2(tt.args.value); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewFromKgfcm2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewFromBar(t *testing.T) {
+	type args struct {
+		value float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want Pressure
+	}{
+		{
+			name: "Should create from PSI properly",
+			args: args{
+				value: 1,
+			},
+			want: Pressure{
+				PSI:    14.50347756,
+				Kgfcm2: 1.01972,
+				Bar:    1,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewFromBar(tt.args.value); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewFromBar() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
