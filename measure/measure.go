517ab8f6607f258aa19e68e9681f4b03fea5be7a
Alan Cesar Elias <alancesarelias@gmail.com>
2021-03-29 20:43:08 -0300

    Fix regex in order to support 'gal' symbol

--- measure/measure.go
+++ measure/measure.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-var regex = regexp.MustCompile(`(\d{1,5}\.?\d*)(\s?)(\D{1,2})`)
+var regex = regexp.MustCompile(`(\d{1,5}\.?\d*)(\s?)(\D{1,3})`)
 
 const (
 	valueIndex  = 1
