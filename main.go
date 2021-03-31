d1d6955422dea4dc24d353e7d6be3f21bdb13521
Alan Cesar Elias <alan.elias@pismo.io>
2021-03-31 18:41:11 -0300

    Add CORS

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/alancesar/homebrew/handler"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 
 func main() {
 	engine := gin.Default()
+	engine.Use(cors.Default())
 	engine.Handle(http.MethodPost, "/recipe", handler.RecipeHandler())
 	if err := engine.Run(":8080"); err != nil {
 		log.Fatal(err)
